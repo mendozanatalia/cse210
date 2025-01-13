86727bbeaf366b2c7ee8d4137ed1c374807309a5
mendozanatalia <nmendoza@byupathway.edu>
2025-01-12 22:48:11 -0400

    ex3 first commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("----- MAGIC NUMBER GAME -----");
+
+        // User specified number
+        // Console.Write("What's the magic number?: ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        int guess = -1;
+
+        // Track how many guesses
+        int attempts = 0;
+
+        // Loop: Play again (Not working yet)
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            // Random Number
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            
+            while (guess != magicNumber)
+            {
+                Console.Write("What's your guess?: ");
+                guess = int.Parse(Console.ReadLine());
+
+                attempts += 1;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higuer");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("----------------------------------");
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"Attempts = {attempts}");
+                    Console.WriteLine("----------------------------------");
+                }
+            }
+            
+            Console.Write("Do you wanna play again? Yes/No: ");
+            string answer = Console.ReadLine();
+            playAgain = playAgain.ToLower();
+            Console.WriteLine("----------------------------------");
+        }
     }
 }
