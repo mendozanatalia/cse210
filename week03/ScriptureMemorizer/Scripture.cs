7fabd801171db7cadf3ced3fb1899ac216ca9a0b
mendozanatalia <nmendoza@byupathway.edu>
2025-01-31 20:58:19 -0400

    classes with attributes, constructors and methods created

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,25 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    
+    public Scripture (Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+    public string GetDisplayText()
+    {
+
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        
+    }
+}
